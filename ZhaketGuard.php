9e670d4b922a258c625e67beff4bd19778dd7a25
Farhad HP <farhadhp.ir@gmail.com>
2020-02-23 13:18:03 +0330

    Create ZhaketGuard class and add api url route

---
+++ ZhaketGuard.php
@@ -0,0 +1,18 @@
+<?php
+
+
+namespace ZhaketGuard;
+
+
+class ZhaketGuard
+{
+
+    /**
+     * Dont change this
+     *
+     * @var string
+     */
+    private $apiUrl = 'http://guard.zhaket.com/api/';
+
+
+}
